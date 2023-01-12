3c6095e093d76f9cb4b5561622415ded5e4aa279
pr26svn <pr26sautin@mail.ru>
2023-01-12 14:55:14 +0600

    solid open-close

--- public/solid/solid_o.php
+++ public/solid/solid_o.php
@@ -1,1 +1,60 @@
 <?php
+interface ISomeO{
+    public function getObjectName();
+    public function getHandler();
+}
+class Object1 implements ISomeO{
+    public function getObjectName(){
+        return 'object_1';
+    }
+    public function getHandler(){
+        return 'handle_object_1';
+    }
+}
+class Object2 implements ISomeO{
+    public function getObjectName(){
+        return 'object_2';
+    }
+    public function getHandler(){
+        return 'handle_object_2';
+    }
+}
+class SomeObject {
+    protected $someObject;
+    
+
+    public function __construct(ISomeO $someObject) { 
+        $this->someObject=$someObject; 
+    }
+
+    public function getObjectName() {
+        return $this->someObject->getObjectName;
+    }
+    public function getHandler()
+    {
+        return $this->someObject->getHandler;
+    }
+}
+
+class SomeObjectsHandler {
+    public function __construct() {
+
+    }
+
+    public function handleObjects(array $objects): array {
+        $handlers = [];
+        foreach ($objects as $object) {
+           $handlers[]=$object->getHandler();
+        }
+
+        return $handlers;
+    }
+}
+
+$objects = [
+    new SomeObject(Object1::class),
+    new SomeObject(Object2::class)
+];
+
+$soh = new SomeObjectsHandler();
+$soh->handleObjects($objects);
